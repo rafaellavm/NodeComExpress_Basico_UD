45a47d86d3392dfd65a755cd61b015871ea81864
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-02-10 14:41:02 -0200

    10. Criando Módulos no Node JS

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 var express = require('express');
+var meuModulo = require('./modulo.externo')();
+
+
+console.log(meuModulo);
+
 var app = express();
 
 app.set('view engine', 'ejs');
---
+++ modulo.externo.js
@@ -0,0 +1,8 @@
+//var teste = "meu módulo externo";
+
+//module.exports = teste;
+
+module.exports = function(){
+    var teste = "meu módulo externo";
+    return teste;
+};
